f543cf72a34f4d3ee56617ed3e94ab5f3b62499c
Erwin Reijnders <emreijnders@gmail.com>
2015-09-11 11:03:00 +0200

    interface change
    removed intersects() because it doesn't belong in bounded, added
    getBoundingRectangle()

--- src/main/java/emr/stuff/Bounded.java
+++ src/main/java/emr/stuff/Bounded.java
@@ -1,9 +1,11 @@
 package emr.stuff;
 
+import java.awt.geom.Rectangle2D;
+
 public interface Bounded
 {
 	public Location getTopLeft();
 	public double getWidth();
 	public double getHeight();
-	public boolean intersects( Bounded other );
+	public Rectangle2D getBoundingRectangle();
 }
